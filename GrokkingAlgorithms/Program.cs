2372f26b16878e79f96a33f2f4ddd481f1fa2621
Александр Жаворонок <C0baltt@live.com>
2023-04-28 21:45:40 +0300

    Алгоритм бинарного поиска

--- GrokkingAlgorithms/Program.cs
+++ GrokkingAlgorithms/Program.cs
@@ -10,6 +10,35 @@ namespace GrokkingAlgorithms
     {
         static void Main(string[] args)
         {
+            int[] array = { 10, 11, 15, 19, 20 };
+
+            int low = 0;
+            int hight = array.Length - 1;
+            int item = 15;
+            int mid = array[0];
+
+            while (low <= hight)
+            {
+                mid = low + hight;
+                int guess = array[mid];
+
+                if (guess == item)
+                {
+                    Console.WriteLine($"Искомый элемент {item} в массиве найден под номером {mid+1}");
+                    break;
+                }
+                else if (guess > item)
+                {
+                    hight = mid - 1;
+                }
+                else 
+                {
+                low = mid + 1;
+                }
+            }
+
+            Console.WriteLine(mid);
+            Console.ReadKey();
         }
     }
 }
