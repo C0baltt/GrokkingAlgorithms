5b82810a96495a6307b58b48107456231c14e270
Александр Жаворонок <C0baltt@live.com>
2023-05-20 14:00:38 +0300

    Вариант с GitHub от jointvirt. Версия на C#

--- GrokkingAlgorithms/Program.cs
+++ GrokkingAlgorithms/Program.cs
@@ -1,78 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace GrokkingAlgorithms
 {
     class Program
     {
-        static void Main(string[] args)
+        public class SelectionSort
         {
-            Random rand = new Random();
-
-            int[] nums = new int[rand.Next(10,16)];
-            int temp;
-            int elementNumberInTheOutput = 1;
-            
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Array:");
-
-            for (int i = 0; i < nums.Length ; i++)
+            public int[] SelectionSorting(List<int> arr)
             {
-                nums[i] = rand.Next(0,50);
+                var newArr = new int[arr.Count];
 
-                WriteArrayElement(i);
-            } 
-
-            for (int i = 0; i < nums.Length - 1; i++)
-            {
-                for (int j = i + 1; j < nums.Length; j++)
+                for (int i = 0; i < newArr.Length; i++)
                 {
-                    if (nums[i] > nums[j])
+                    var smallest = FindSmallest(arr);
+                    newArr[i] = arr[smallest];
+                    arr.RemoveAt(smallest);
+                }
+                return newArr;
+            }
+
+            private static int FindSmallest(List<int> arr)
+            {
+                var smallest = arr[0];
+                var smallestIndex = 0;
+
+                for (int i = 0; i < arr.Count; i++)
+                {
+                    if (arr[i] < smallest)
                     {
-                        temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
+                        smallest = arr[i];
+                        smallestIndex = i;
                     }
-
                 }
-            }
-            
-            elementNumberInTheOutput = 1;
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("\nSorting array:");
 
-            for (int i = 0; i < nums.Length; i++)
-            {
-                WriteArrayElement(i);
-            }
-
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"\nThe number of array elements: {nums.Length}\n");
-
-            Console.ReadKey();
-
-            void WriteArrayElement(int index)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.Write($"{index}: ");
-                
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write($"{nums[index]}");
-                
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(" || ");
-
-                if (elementNumberInTheOutput > 9)
-                {
-                    elementNumberInTheOutput=1;
-                    Console.WriteLine();
-                }
-                elementNumberInTheOutput++;
-                   // Console.WriteLine();
+                return smallestIndex;
             }
         }
+        //static void Main(string[] args)
+        //{
+
+        //}
     }
 }
